317a258c660f65f3e556ce66d58665d8e6284dc0
winmo <win.moi@hotmail.com>
2020-03-09 09:00:12 +0100

    Réel Mutex

--- Bac2Q2Threads/Program.cs
+++ Bac2Q2Threads/Program.cs
@@ -6,12 +6,18 @@ namespace Bac2Q2Threads
     internal class Program
     {
         private static int iCommun;
-        // mutex sous forme de file, le suivant à demander sera mis en file d'attente FIFO
         // permet de bloquer les threads qui se partagent ce même verrou et laisser libres les autres => moins contraignant que le lock
-        private static ReaderWriterLock readerWriterLock = new ReaderWriterLock(); 
+        // latence très importante par rapport au lock classique (mais utile lors de tâches lourdes)
+        private static ReaderWriterLock readerWriterLock;
+        
+        // mutex sous forme de file, le suivant à demander sera mis en file d'attente FIFO
+        // tmps calcul un chouia plus long mais négligeable
+        private static Mutex verrou;
         
         public static void Main(string[] args)
         {
+            readerWriterLock = new ReaderWriterLock();
+            verrou = new Mutex();
             Thread a = new Thread(A);
             Thread b = new Thread(B);
             
@@ -31,13 +37,15 @@ namespace Bac2Q2Threads
             
             for (int i = 0; i < 500; i++)
             {
-                readerWriterLock.AcquireWriterLock(-1);
+                //readerWriterLock.AcquireWriterLock(-1);
+                verrou.WaitOne();
                 n = iCommun;
                 n++;
                 Console.Write("A{0, -5}", iCommun); // -5 => codé sur 5 caractères, remplit le reste avec des espaces
 
                 iCommun = n;
-                readerWriterLock.ReleaseWriterLock();
+                verrou.ReleaseMutex();
+                //readerWriterLock.ReleaseWriterLock();
             }
         }
 
@@ -47,13 +55,15 @@ namespace Bac2Q2Threads
             
             for (int i = 0; i < 500; i++)
             {
-                readerWriterLock.AcquireWriterLock(-1);
+                //readerWriterLock.AcquireWriterLock(-1);
+                verrou.WaitOne();
                 n = iCommun;
                 n++;
                 Console.Write("B{0, -5}", iCommun);
                 
                 iCommun = n;
-                readerWriterLock.ReleaseWriterLock();
+                verrou.ReleaseMutex();
+                //readerWriterLock.ReleaseWriterLock();
             }
         }
     }
