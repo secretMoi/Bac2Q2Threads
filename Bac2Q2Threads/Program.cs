9dd183f02b3a49eb3bf6db47b7d8fb6e5c8c79e4
winmo <win.moi@hotmail.com>
2020-03-09 08:51:25 +0100

    Programme avec verrou basique

---
+++ Bac2Q2Threads/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Threading;
+
+namespace Bac2Q2Threads
+{
+    internal class Program
+    {
+        private static int iCommun;
+        
+        public static void Main(string[] args)
+        {
+            Thread a = new Thread(A);
+            Thread b = new Thread(B);
+            
+            a.Start();
+            b.Start();
+
+            a.Join();
+            b.Join();
+            
+            Console.WriteLine("FINI");
+            Console.ReadLine();
+        }
+        
+        private static void A()
+        {
+            int n;
+            
+            for (int i = 0; i < 500; i++)
+                lock (typeof(Program))
+                {
+                    n = iCommun;
+                    n++;
+                    Console.Write("A{0, -5}", iCommun); // -5 => codé sur 5 caractères, remplit le reste avec des espaces
+
+                    iCommun = n;
+                }
+        }
+
+        private static void B()
+        {
+            int n;
+            
+            for (int i = 0; i < 500; i++)
+                lock (typeof(Program))
+                {
+                    n = iCommun;
+                    n++;
+                    Console.Write("B{0, -5}", iCommun);
+                    
+                    iCommun = n;
+                }
+        }
+    }
+}
